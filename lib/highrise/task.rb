f76e9953407a76eba9a8522babba574bc6bd7ef2
M. Scott Ford <scott@vaderpi.com>
2010-02-08 23:47:51 -0500

    Improved documentation for Highrise::Task.

--- lib/highrise/task.rb
+++ lib/highrise/task.rb
@@ -1,9 +1,9 @@
 module Highrise
+  # Access upcoming, assigned and completed tasks by calling find with the corresponding :from value.
+  #    find(:all, :from => :upcoming)
+  #    find(:all, :from => :assigned)  
+  #    find(:all, :from => :completed)  
   class Task < Base
-    # find(:all, :from => :upcoming)
-    # find(:all, :from => :assigned)  
-    # find(:all, :from => :completed)  
-
     def complete!
       load_attributes_from_response(post(:complete))
     end
